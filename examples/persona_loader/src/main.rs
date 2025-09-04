cd5d96acbf7a6e94097f84c10b1ee483d040b0af
Pheonetic Coder <79929721+lazyxeon@users.noreply.github.com>
2025-09-04 16:32:38 -0400

    Implement persona loading from ZIP file

---
+++ examples/persona_loader/src/main.rs
@@ -0,0 +1,6 @@
+use astraweave_persona::load_persona_zip;
+fn main() -> anyhow::Result<()> {
+    let p = load_persona_zip("sniper_persona.zip")?;
+    println!("Loaded persona: tone={}, verify={}", p.persona.tone, p.verify());
+    Ok(())
+}
