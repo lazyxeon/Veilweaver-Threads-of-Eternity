086860344a3903e7f28e9f86cfd445482720f2e7
Pheonetic Coder <79929721+lazyxeon@users.noreply.github.com>
2025-09-04 18:44:44 -0400

    Add ResourceNode struct with harvesting functionality

---
+++ astraweave-gameplay/src/harvesting.rs
@@ -0,0 +1,34 @@
+use serde::{Serialize, Deserialize};
+use glam::Vec3;
+use crate::{ResourceKind, Inventory};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ResourceNode {
+    pub kind: ResourceKind,
+    pub pos: Vec3,
+    pub amount: u32,
+    pub respawn_time: f32,
+    pub timer: f32,
+}
+
+impl ResourceNode {
+    pub fn harvest(&mut self, inv: &mut Inventory, n: u32) -> u32 {
+        let take = n.min(self.amount);
+        self.amount -= take;
+        inv.add_resource(self.kind, take);
+        if self.amount == 0 {
+            self.timer = self.respawn_time;
+        }
+        take
+    }
+
+    pub fn tick(&mut self, dt: f32) {
+        if self.amount == 0 {
+            self.timer -= dt;
+            if self.timer <= 0.0 {
+                self.amount = 1 + (3 * rand::random::<u8>() as u32 % 5);
+                self.timer = 0.0;
+            }
+        }
+    }
+}
