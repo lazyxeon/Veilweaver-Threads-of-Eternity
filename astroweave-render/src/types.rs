9aeef4b8ae0abae6687856c728973ea2ce45dbfa
Pheonetic Coder <79929721+lazyxeon@users.noreply.github.com>
2025-09-04 17:45:47 -0400

    Add Vertex and Instance structures for rendering

---
+++ astroweave-render/src/types.rs
@@ -0,0 +1,94 @@
+use bytemuck::{Pod, Zeroable};
+use glam::{Mat4, Vec3};
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable)]
+pub struct Vertex {
+    pub position: [f32; 3],
+    pub normal:   [f32; 3],
+}
+
+impl Vertex {
+    pub const ATTRS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![
+        0 => Float32x3, // position
+        1 => Float32x3, // normal
+    ];
+
+    pub fn layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRS,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable)]
+pub struct InstanceRaw {
+    pub model: [[f32; 4]; 4],
+    pub normal_matrix: [[f32; 3]; 3],
+    pub color: [f32; 4],
+}
+
+impl InstanceRaw {
+    pub fn layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        use std::mem::size_of;
+        wgpu::VertexBufferLayout {
+            array_stride: size_of::<InstanceRaw>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &[
+                // model matrix (4x vec4)
+                wgpu::VertexAttribute { offset: 0,  shader_location: 2,  format: wgpu::VertexFormat::Float32x4 },
+                wgpu::VertexAttribute { offset: 16, shader_location: 3,  format: wgpu::VertexFormat::Float32x4 },
+                wgpu::VertexAttribute { offset: 32, shader_location: 4,  format: wgpu::VertexFormat::Float32x4 },
+                wgpu::VertexAttribute { offset: 48, shader_location: 5,  format: wgpu::VertexFormat::Float32x4 },
+                // normal matrix (3x vec3 packed as Float32x3)
+                wgpu::VertexAttribute { offset: 64, shader_location: 6,  format: wgpu::VertexFormat::Float32x3 },
+                wgpu::VertexAttribute { offset: 76, shader_location: 7,  format: wgpu::VertexFormat::Float32x3 },
+                wgpu::VertexAttribute { offset: 88, shader_location: 8,  format: wgpu::VertexFormat::Float32x3 },
+                // color
+                wgpu::VertexAttribute { offset: 100, shader_location: 9, format: wgpu::VertexFormat::Float32x4 },
+            ],
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Mesh {
+    pub vertex_buf: wgpu::Buffer,
+    pub index_buf:  wgpu::Buffer,
+    pub index_count: u32,
+}
+
+#[derive(Clone, Debug)]
+pub struct Material {
+    pub color: [f32; 4],
+}
+
+#[derive(Clone, Debug)]
+pub struct Instance {
+    pub transform: Mat4,
+    pub color: [f32; 4],
+}
+
+impl Instance {
+    pub fn raw(&self) -> InstanceRaw {
+        let model = self.transform;
+        let normal = model.inverse().transpose();
+        InstanceRaw {
+            model: model.to_cols_array_2d(),
+            normal_matrix: [
+                normal.x_axis.truncate().to_array(),
+                normal.y_axis.truncate().to_array(),
+                normal.z_axis.truncate().to_array(),
+            ],
+            color: self.color,
+        }
+    }
+
+    pub fn from_pos_scale_color(pos: Vec3, scale: Vec3, color: [f32; 4]) -> Self {
+        let transform = Mat4::from_scale_rotation_translation(scale, glam::Quat::IDENTITY, pos);
+        Self { transform, color }
+    }
+}
