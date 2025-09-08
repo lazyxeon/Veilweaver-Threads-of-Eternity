1449c3830b438ec416371c8bbef9ed9086fb0fb2
github-action-benchmark <github@users.noreply.github.com>
2025-09-08 22:25:30 +0000

    add Rust Benchmark (cargo) benchmark result for 1dfdf7c731372dcbea936c0e15fd88869e77c6e4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757369896137,
+  "lastUpdate": 1757370330826,
   "repoUrl": "https://github.com/lazyxeon/Veilweaver-Threads-of-Eternity",
   "entries": {
     "Rust Benchmark": [
@@ -251,6 +251,68 @@ window.BENCHMARK_DATA = {
             "name": "binding_set_creation",
             "value": 676,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "lazyxeon",
+            "username": "lazyxeon"
+          },
+          "committer": {
+            "name": "lazyxeon",
+            "username": "lazyxeon"
+          },
+          "id": "1dfdf7c731372dcbea936c0e15fd88869e77c6e4",
+          "message": "[WIP] please analyze and develop badges bespoke for my repo to insert into my readme",
+          "timestamp": "2025-09-08T21:52:22Z",
+          "url": "https://github.com/lazyxeon/Veilweaver-Threads-of-Eternity/pull/30/commits/1dfdf7c731372dcbea936c0e15fd88869e77c6e4"
+        },
+        "date": 1757370330100,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "world_creation",
+            "value": 56,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "entity_spawning",
+            "value": 23431,
+            "range": "± 138",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "world_tick",
+            "value": 48,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_creation",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_serialization",
+            "value": 64,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_deserialization",
+            "value": 96,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_set_creation",
+            "value": 672,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
