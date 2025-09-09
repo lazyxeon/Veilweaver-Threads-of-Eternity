45a7a2b08f44066b12107be70178dd185bb43b58
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 14:13:40 +0000

    add Rust Benchmark (cargo) benchmark result for 7650bb2e68d4b2594b8e336573adb18d83fe15f0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757427208326,
+  "lastUpdate": 1757427220467,
   "repoUrl": "https://github.com/lazyxeon/AstraWeave-AI-Native-Gaming-Engine",
   "entries": {
     "Rust Benchmark": [
@@ -61,6 +61,68 @@ window.BENCHMARK_DATA = {
             "name": "binding_set_creation",
             "value": 663,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "lazyxeon",
+            "username": "lazyxeon"
+          },
+          "committer": {
+            "name": "lazyxeon",
+            "username": "lazyxeon"
+          },
+          "id": "7650bb2e68d4b2594b8e336573adb18d83fe15f0",
+          "message": "Add comprehensive executive summary and strategic pitch materials for AstraWeave",
+          "timestamp": "2025-09-08T23:36:03Z",
+          "url": "https://github.com/lazyxeon/AstraWeave-AI-Native-Gaming-Engine/pull/36/commits/7650bb2e68d4b2594b8e336573adb18d83fe15f0"
+        },
+        "date": 1757427219195,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "world_creation",
+            "value": 56,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "entity_spawning",
+            "value": 23804,
+            "range": "± 243",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "world_tick",
+            "value": 54,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_creation",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_serialization",
+            "value": 67,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_deserialization",
+            "value": 101,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "binding_set_creation",
+            "value": 663,
+            "range": "± 12",
             "unit": "ns/iter"
           }
         ]
